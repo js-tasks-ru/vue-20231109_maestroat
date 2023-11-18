57f2272a74dfca69194b94d9b9916a9627c12041
antontimofeev <Maestroat@yandex.ru>
2023-11-18 20:34:33 +0300

    two

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      operation: null,
+      numb_1: null,
+      numb_2: null,
+      calc: 0
+    };
+  },
+  computed: {
+    funcCalc() {
+      switch (this.operation) {
+        case 'sum':
+          this.calc = this.numb_1 + this.numb_2
+          break;
+        case 'subtract':
+          this.calc = this.numb_1 - this.numb_2
+          break;
+        case 'multiply':
+          this.calc = this.numb_1 * this.numb_2
+          break;
+        case 'divide':
+          this.calc = this.numb_1 / this.numb_2
+          break;
+        default:
+          this.calc = 0
+          break;
+      }
+    },
+  },
+})
+
+const app = createApp(App);
+const vm = app.mount('#app');
+
+

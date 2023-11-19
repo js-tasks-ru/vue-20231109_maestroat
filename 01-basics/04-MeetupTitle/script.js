6586e482a36cb10e21afeb279b441874559dff16
antontimofeev <Maestroat@yandex.ru>
2023-11-19 19:25:31 +0300

    four

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent} from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,23 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+    name: 'App',
+    data() {
+      return {
+        elements: 5,
+        value: null,
+        info: null
+      };
+    },
+  watch: {
+    value(newValue, oldValue) {}
+  },
+    async updated() {
+      this.info = await fetchMeetupById(this.value);
+    }
+  }
+)
+
+const app = createApp(App);
+const vm = app.mount('#app');

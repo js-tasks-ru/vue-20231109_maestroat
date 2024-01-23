ed633fc32895e47a0b29af6a9fe73936fc0254d5
antontimofeev <Maestroat@yandex.ru>
2024-01-23 23:32:47 +0300

    wrappers_2_3

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,5 @@
 <template>
-  <UiInput :type="type" :step="step" v-bind="$attrs" @input="proxy($event)" :modelValue="val">
+  <UiInput :type="type" :step="step" @input="proxy($event)" :modelValue="val">
     <template v-for="slotName of Object.keys($slots)" #[slotName]>
       <slot :name="slotName" />
     </template>
@@ -10,8 +10,6 @@
 import UiInput from './UiInput.vue';
 export default {
   name: 'UiInputDate',
-  inheritAttrs: false,
-
   components: { UiInput },
   props: {
     type: {
@@ -28,44 +26,29 @@ export default {
   emits: {
     'update:modelValue': [Number, null],
   },
-  data() {
-    return {
-      val: '',
-    };
-  },
-  watch: {
-    modelValue: {
-      immediate: true,
-      handler() {
-        const format = (date) => (date < 10 ? `0${date}` : date.toString());
-        const date = new Date(this.modelValue);
-        if (this.type === 'date') {
-          const day = date.getUTCDate();
-          const month = date.getUTCMonth() + 1;
-          const year = date.getUTCFullYear();
-          this.val = `${year}-${format(month)}-${format(day)}`;
-        }
-        if (this.type === 'time') {
-          // const h1 = date.getHours();
-          const h2 = date.getUTCHours();
-          const m = date.getMinutes();
-          this.val = `${h2}:${m}`;
-        }
-        if (this.type === 'datetime-local') {
-          const day = date.getUTCDate();
-          const month = date.getUTCMonth() + 1;
-          const year = date.getUTCFullYear();
-          // const h1 = date.getHours();
-          const h2 = date.getUTCHours();
-          const m = date.getMinutes();
-          this.val = `${year}-${format(month)}-${format(day)}T${h2}:${m}`;
-        }
-      },
+  computed: {
+    val() {
+      const format = (date) => (date < 10 ? `0${date}` : date.toString());
+      const date = new Date(this.modelValue);
+      const day = date.getUTCDate();
+      const month = date.getUTCMonth() + 1;
+      const year = date.getUTCFullYear();
+      const h = date.getUTCHours();
+      const m = date.getUTCMinutes();
+      if (this.type === 'date') {
+        return `${year}-${format(month)}-${format(day)}`;
+      }
+      if (this.type === 'time') {
+        return `${format(h)}:${format(m)}`;
+      }
+      if (this.type === 'datetime-local') {
+        return `${year}-${format(month)}-${format(day)}T${format(h)}:${format(m)}`;
+      }
+      return '';
     },
   },
   methods: {
     proxy(e) {
-      // console.log(e.target.valueAsNumber);
       this.$emit('update:modelValue', e.target.valueAsNumber);
     },
   },

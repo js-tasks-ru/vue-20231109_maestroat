5c82f5c2d60840fc990fa9e0b06aad822134581d
antontimofeev <Maestroat@yandex.ru>
2024-02-13 22:00:52 +0300

    07_forms_3

--- 08-reactivity/04-debouncedRef/utils/debouncedRef.js
+++ 08-reactivity/04-debouncedRef/utils/debouncedRef.js
@@ -8,14 +8,7 @@ import debounce from 'lodash/debounce';
  * @returns {Ref<T>} - Новый ref, обновляющийся с debounce при обновлении исходного ref-а
  */
 export function debouncedRef(source, wait) {
-  const debounced = ref(source.value);
-
-  watch(
-    () => source.value,
-    debounce((newValue) => {
-      debounced.value = newValue;
-    }, wait),
-  );
-
+  const debounced = ref(undefined); // ...
+  // ...
   return debounced;
 }
